79957f3b575753586fabdbbd0df04d1ea328687c
David Soto Sousa <sousavlc86@gmail.com>
2023-03-21 00:42:52 +0100

    feat: Intento de ver detalle de los usuarios

--- src/layout/detail/Detail.jsx
+++ src/layout/detail/Detail.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { detailData } from '../detailSlice';
+
+import './Detail.css';
 
 export const Detail = () => {
+
+  //Conexion a RDX en modo lectura
+  const detailRedux = useSelector(detailData);
+
+  useEffect(()=>{
+    console.log(detailRedux.choosenObject?.data.name, "potatoe")
+  },[]);
+
   return (
-    <div>Detail</div>
+    <Container fluid className='detailDesign'>
+            <Row>
+                <Col>
+                    {detailRedux?.choosenObject?.data.name}
+                </Col>
+                {/* <Col>
+                    {detailRedux?.choosenObject?.email}
+                </Col> */}
+            </Row>
+        </Container>
   )
 }

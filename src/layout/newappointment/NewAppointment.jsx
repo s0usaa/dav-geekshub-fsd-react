698d62690046449d87160324dafd096cde7f028e
David Soto Sousa <sousavlc86@gmail.com>
2023-04-05 14:14:12 +0200

    feat:DatePicker probando crear citas

--- src/layout/newappointment/NewAppointment.jsx
+++ src/layout/newappointment/NewAppointment.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { Col, Container, Form, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { InputText } from "../../components/inputtext/InputText";
 import { createCitas } from "../../services/apiCalls";
 import { userData } from "../userSlice";
 import "./NewAppointment.css";
+import dayjs from "dayjs";
+import setMinutes from "date-fns/setMinutes";
+import setHours from "date-fns/setHours";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export const NewAppointment = () => {
   const navigate = useNavigate();
   const [welcome, setWelcome] = useState("");
+  const [date, setDate] = useState(null)
   const reduxCredentials = useSelector(userData);
+  const [horas, setHoras] = useState(setHours(setMinutes(new Date(),0),9));
+
+  // const [doctor, setDoctor] = useState("");
   
   const [newAppointment, setNewAppointment] = useState({
     doctor_id: "",
@@ -18,12 +27,16 @@ export const NewAppointment = () => {
     hour: "",
   });
 
+
+
   const inputHandler = (e) => {
     setNewAppointment((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
   };
+
+  console.log(newAppointment);
 
   const createAppointment = () => {
     createCitas(newAppointment, reduxCredentials.credentials.token)
@@ -68,25 +81,46 @@ export const NewAppointment = () => {
             </Form.Group>
             <Form.Group className="mb-1" controlId="formBasicDate">
               <Form.Label>Fecha</Form.Label>
-              <InputText
+              <DatePicker
+              selected={date}
+              onChange={(fecha)=>setDate(fecha)}
+              dateFormat="dd/MM/yyyy"
+              timeInputLabel="Fecha:"
+              minDate={new Date()}
+              placeholderText="Selecciona una fecha"
+              />
+              {/* <InputText
                 className={""}
                 type={"date"}
                 name={"date"}
                 placeholder={"Selecciona tu doctor"}
                 changeFunction={(e) => inputHandler(e)}
                 blurFunction={(e)=> (e)}
-              />
+              /> */}
             </Form.Group>
             <Form.Group className="mb-1" controlId="formBasicHour">
               <Form.Label>Hora</Form.Label>
-              <InputText
+              <DatePicker
+              selected={horas}
+              onChange={(hora) => setHoras(hora)}
+              showTimeSelect
+              showTimeSelectOnly
+              timeIntervals={30}
+              timeCaption="Hora"
+              timeFormat="HH:mm"
+              minTime={setHours(setMinutes(new Date(),0),9)}
+              maxTime={setHours(setMinutes(new Date(),30), 20)}
+              dateFormat="HH:mm"
+              placeholderText="Selecciona una hora"
+              />
+              {/* <InputText
                 className={""}
                 type={"time"}
                 name={"hour"}
                 placeholder={"Selecciona tu doctor"}
                 changeFunction={(e) => inputHandler(e)}
                 blurFunction={(e)=> (e)}
-              />
+              /> */}
             </Form.Group>
             <div className="loginSendDeac loginSendAct m-3" onClick={createAppointment}>
               Pedir Cita
